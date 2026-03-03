d6256c181dc806dc5dc417023421a7e3c4e799af
robertopiarulli-wq <roberto.piarulli@gmail.com>
2026-03-03 10:58:03 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "../football_quantum_v3.jsx"
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
